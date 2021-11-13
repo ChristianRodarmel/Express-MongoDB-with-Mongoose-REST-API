51b9f270b594ac86146cec6c3e3c461728c8dd79
ChristianRodarmel <christian.rodarmel1@gmail.com>
2021-11-13 12:02:52 -0800

    Updated server working here with Postman app

--- app.js
+++ app.js
@@ -38,9 +38,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('./campsites', campsiteRouter);
-app.use('./promotions', promotionRouter);
-app.use('./partners', partnerRouter);
+app.use('/campsites', campsiteRouter);
+app.use('/promotions', promotionRouter);
+app.use('/partners', partnerRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
